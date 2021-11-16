9ca0ea855675180f048a3e790410fd2f89195b90
aidana21 <93640059+aidana21@users.noreply.github.com>
2021-11-16 20:25:23 +0600

    Update aidana.php

--- aidana.php
+++ aidana.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Aidana</title>
+  <link rel="stylesheet"  href="main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <img src="https://th.bing.com/th/id/R.9d188ffd3a511d4694a7dc2bd7988fe2?rik=x9zF1JoHQE3mgQ&pid=ImgRaw&r=0" alt="" width="500" height="500">
+<div class="WholePort">
+  <img src="https://i.pinimg.com/564x/a0/1a/e8/a01ae835831e6951a202b9b1c5d4ac91.jpg" alt="Me">
+  <p>Айдана, 17 лет учусь в колледже Ала-Тоо на танцы и курсы английского.<br>В свободное время люблю гулять и смотреть сериалы<br>
+  Aдрес- Московская 85 Номер- 0707-78-18-08<br></p>
+
+</div>
+<div class="Icons">
+  <a href="https://www.youtube.com/" class="fa fa-facebook"></a>
+  <a href="#" class="fa fa-instagram"></a>
+  <a href="#" class="fa fa-youtube"></a>
+</div>
+
+
 </body>
-</html> 
+</html>
